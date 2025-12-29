102eea43a7b1e64603e107eb6a9dde284931ad14
Doc-Maxx <maxx.miller95@gmail.com>
2025-12-29 14:47:46 -0500

    finishing wall object

--- functions/functions.py
+++ functions/functions.py
@@ -6,10 +6,18 @@ class space:
         self.connections = connections
 
 class wall:
-    def __init__(self, point_1, point_2,thickness):
+    def __init__(self, point_1, point_2, thickness):
         self.points = [point_1, point_2]
         self.thickness = thickness
-        self.ori = 
+        self.ori = self.diff*j
         self.diff = point_2 - point_1
         self.slope = np.imag(self.diff) / np.real(self.diff)
-        
+        self.y_intercept = np.imag(point_1) - self.slope*np.real(point_1)
+        self.x_intercept = np.imag(point_1) / self.slope - np.real(point_1)
+        self.shift = self.shift_comp
+    
+    def shift_comp(self):
+        if self.y_intercept < self.x_intercept:
+            return 0 + self.y_intercept*j
+        else:   
+            return self.x_intercept + 0j
