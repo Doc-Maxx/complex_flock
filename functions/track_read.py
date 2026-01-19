0a986ee1ae0ca9b8ec30e3aeab370a17c7bee7dd
Doc-Maxx <maxx.miller95@gmail.com>
2026-01-19 10:11:22 -0500

    finished the data file reader, it will output a region list to be passed into the space object. yet to debug.

--- functions/track_read.py
+++ functions/track_read.py
@@ -2,6 +2,7 @@ import csv
 import sys
 sys.path.append("./functions")
 import functions as fn
+import numpy as np
 
 def read_track(file):
     with open("./tracks/"+file+".txt", 'r') as f:
@@ -23,5 +24,36 @@ def region_builder(reader):
             regions.append(new)
     return regions
 
+def rect_row_reader(row):
+    origin = complex(row[1].replace(" ", ""))
+    p2 = complex(row[2].replace(" ", ""))
+    thickness = float(row[3])
+    bound = str_to_bool(row[4])
+    return fn.rectangle(origin = origin, point_2=p2, thickness = thickness, boundary_bool=bound)
+
+def circle_row_reader(row):
+    origin = complex(row[1].replace(" ", ""))
+    radius = float(row[2])
+    arc = np.array(degree_to_radian(float(row[3])), degree_to_radian(float(row[4])))
+    bound = str_to_bool(row[5])
+    return fn.circle(origin=origin, radius=radius, arc = arc, boundary_bool=bound)
+def ring_row_reader(row):
+    origin = complex(row[1].replace(" ", ""))
+    radius_i = float(row[2])
+    radius_o = float(row[3])
+    arc = np.array(degree_to_radian(float(row[4])), degree_to_radian(float(row[5])))
+    bound = str_to_bool(row[6])
+    return fn.ring(origin=origin, radius_inner= radius_i, radius_outer=radius_o, arc = arc, boundary_bool=bound)
+
+def degree_to_radian(ang):
+    return ang * (np.pi / 360)
+
+def str_to_bool(str):
+    if str == "True":
+        return True
+    elif str == "False":
+        return False
+    else:
+        print("Error: Bool passed might not be capitalized")    
 
 read_track(file = "race_track")
