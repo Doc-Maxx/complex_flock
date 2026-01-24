2ffdc8c10805660995637dfc8d4531030ae98479
Doc-Maxx <maxx.miller95@gmail.com>
2026-01-24 10:28:17 -0500

    added a flocker plotting function, but to get it to work with an animation, I'll need to think about it more

--- functions/plotting.py
+++ functions/plotting.py
@@ -5,15 +5,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Wedge
+import matplotlib.animation as animation
 
-
-def plot(space):
-    sb = space.boundary_regions
+def plot(space, dpi = 100):
     fig, ax = plt.subplots()
-    for i in sb:
+    for i in space.boundary_regions:
         add_line(i, ax)
     ax.autoscale_view()
-    fig.savefig("./figs/test.png",dpi=80)
+    fig.savefig("./figs/test.png",dpi)
+
+def plot_flockers(space, axes):
+    pr = np.array([np.real(space.manifest.pos_master)])
+    pi = np.array([np.imag(space.manifest.pos_master)])
+    ang = np.angle(space.manifest.vel_master)
+    axes.scatter(pr,pi)
 
 def add_line(region, axes):
     patches = []
@@ -23,7 +28,7 @@ def add_line(region, axes):
     elif region.type == "circle":
         patches += [
                   Wedge((region.origin.real,region.origin.imag),
-                        region.radius, rtd(region.arc[0]), rtd(region.arc[1]),width=-0.03)
+                        region.radius, rtd(region.arc[0]), rtd(region.arc[1]),width=0.03)
                   ]
         p = PatchCollection(patches)
         axes.add_collection(p)
@@ -36,6 +41,7 @@ def add_line(region, axes):
         axes.add_collection(p)
     else:
         print("Region type not recognized")
+
 def rtd(ang):
     return ang * 360 / (2*np.pi)
 
