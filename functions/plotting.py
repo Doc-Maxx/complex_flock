41f8f7fb52f2c05fa57343e3eb6e5f5577d52923
neptune <maxxmiller@tutanota.com>
2026-02-03 12:48:57 -0500

    updated plotting functions with gen2 versions of the functions able to handle the lines.

--- functions/plotting.py
+++ functions/plotting.py
@@ -49,3 +49,23 @@ def add_line(region, axes):
 def rtd(ang):
     return ang * 360 / (2*np.pi)
 
+def plot_gen2(manifest, file=time.time(),dpi = 100):
+    fig, ax = plt.subplots()
+    for i in manifest.lines:
+        add_line_gen2(i, ax)
+    q = plot_flockers_gen2(space,ax)
+    ax.autoscale_view()
+    fig.savefig("./figs/"+str(file)+".png", dpi=dpi)
+
+def add_line_gen2 = (line, axes):
+    axes.plot(np.real(line.x), np.imag(line.x))
+
+def plot_flockers_gen2(manifest, axes):
+    pr = np.array([np.real(manifest.pos)])
+    pi = np.array([np.imag(manifest.pos)])
+    vr = np.array([np.real(manifest.vel)])
+    vi = np.array([np.imag(manifest.vel)]) 
+    ang = np.angle(manifest.vel)
+    quiv = axes.quiver(pr, pi,vr,vi, (ang /(2*np.pi)))
+    return quiv
+2
