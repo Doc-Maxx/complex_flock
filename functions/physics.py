45f365fdfb4a61e1b678db7f9cd2f69ecea13b1d
neptune <maxxmiller@tutanota.com>
2026-02-03 11:19:28 -0500

    writing an updated collision system using only lines in physics.py

---
+++ functions/physics.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+def i_dot(vec1, vec2):
+    return vec1 * np.conjugate(vec2)
+
+def reflect(vec, mirror_vec):
+    rot_angle = np.angle(mirror_vec) - np.angle(vec)
+    return vec * e**(1j * rot_angle * 2)
+
+class line:
+    def __init__(self, x1, x2, corner_radius):
+        self.x = np.array([x1,x2])
+        self.mag_x = np.abs(x)
+        self.tangent = (self.x[1] - self.x[2])/np.abs(self.x[1] - self.x[2])
+        self.normal = self.make_normal()
+        self.region_projection = i_dot(self.x, self.tangent)
+        self.angle = self.angle()
+        self.R_c = corner_radius
+
+    def make_normal(self):
+        return self.tangent*1j
+
+    def angle(self):
+        return = np.angle(self.tangent)
+
+    def get_region_masks(self, vec):
+        p = i_dot(vec, self.tangent)
+        r0_mask = p < self.region_projection[0]
+        r1_mask = self.region_projection[0] < p < self.region_projection[1]
+        r2_mask = self.region_projection[1] < p
+        return r0_mask, r1_mask, r2_mask
+
+    def distance(self, vec):
+        d = np.array([])
+        masks = self.get_region_masks(vec)
+        for i in np.where(masks[0]):
+            d[i] = vec - self.x[0]
+        for i in np.where(masks[1]):
+            d[i] = i_dot(vec, self.normal)
+        for i in np.where(masks[2]):
+            d[i] = vec - self.x[1]
+        return d, masks
+
+    def get_push_masks(self, distances, region_masks):
+        masks = region_masks
+        for i in np.where(region_masks[0]):
+            if np.abs(distances[i]) > 2*self.corner_radius:
+                masks[0][i] = False
+        for i in np.where(region_masks[1]):
+            if distances[i] > 0+self.corner_radius:
+                masks[1][i] = False
+        for i in np.where(region_masks[2]):
+            if np.mag(distances[i]) > 2*self.corner_radius
+                masks[2][i] = False
+        return masks
+
+    def push(self, vec):
+        distances, region_masks = self.distances(vec)
+        masks = self.get_push_masks(distances, region_masks)
+        for i in vec[masks[0]]:
+            self.corner_push(i)
+
+    def corner_push(self, pos, vel, mask, distances):
+        overlap = 2* self.corner_radius - np.abs(distances)
+        overlap_dir = distances / np.abs(distances)
+        for i in pos[mask]:
+            tangent = overlap_dir[i]*-1j
+            vel[i] = reflect(i, tangent)
+            pos[i] = pos[i] + overlap[i] * overlap_dir[i]
+
+    def line_push(self, pos, vel, mask, distances):
+        for i in pos[mask]:
+            vec[i] = reflect(i, self.tangent)
+            pos[i] = reflect(pos[i]-self.x[0], self.tangent) + self.x[0]
+
+
+
+
